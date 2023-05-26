7db4dd79def581d263613f1a5f20da7bebf025c5
Filipe Santos Correa <mail@filipesantoscorrea.com>
2023-05-26 18:20:23 +0200

    Add useFormatLocalizePrice test

---
+++ app/hooks/useFormatLocalizePrice.test.tsx
@@ -0,0 +1,53 @@
+import { renderHook } from "@testing-library/react";
+import { useFormatLocalizePrice } from "./useFormatLocalizePrice";
+
+describe("useFormatLocalizePrice", () => {
+  it("should format and localize the price correctly", () => {
+    const { result } = renderHook(() =>
+      useFormatLocalizePrice({
+        amount: 12345.6789,
+        currency: "USD",
+      })
+    );
+
+    expect(result.current).toBe("$12,346");
+  });
+
+  it("should update the formatted price when amount or currency changes", () => {
+    const { result, rerender } = renderHook(
+      ({ amount, currency }: { amount: number; currency: string }) =>
+        useFormatLocalizePrice({
+          amount,
+          currency,
+        }),
+      {
+        initialProps: {
+          amount: 12345.6789,
+          currency: "USD",
+        },
+      }
+    );
+
+    expect(result.current).toBe("$12,346");
+
+    rerender({
+      amount: 9876.54321,
+      currency: "EUR",
+    });
+
+    expect(result.current).toBe("€9,877");
+  });
+
+  it("should return an empty string if navigator is undefined", () => {
+    Object.defineProperty(global, "navigator", { value: undefined });
+
+    const { result } = renderHook(() =>
+      useFormatLocalizePrice({
+        amount: 12345.6789,
+        currency: "USD",
+      })
+    );
+
+    expect(result.current).toBe("");
+  });
+});
