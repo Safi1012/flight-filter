0898f32509bbe62a8896a7ff8b665cad51cafd98
Filipe Santos Correa <mail@filipesantoscorrea.com>
2023-05-26 23:02:33 +0200

    Refactor FlightSearch

--- app/components/FlightSearch.tsx
+++ app/components/FlightSearch.tsx
@@ -1,75 +1,56 @@
 "use client";
 
-import {
-  Card,
-  CardBody,
-  FormControl,
-  FormLabel,
-  Heading,
-  Select,
-} from "@chakra-ui/react";
+import { useState } from "react";
+import { Card, CardBody, Heading, Text } from "@chakra-ui/react";
 import { FlightCard } from "./FlightCard";
-
-import { RiArrowDownSFill } from "react-icons/ri";
+import FlightFilter from "./FlightFilter";
 
 interface FlightSearchProps {
   priceOffers: PriceOffer[];
 }
 
 export default function FlightSearch({ priceOffers }: FlightSearchProps) {
+  const [filterByOrigin, setFilterByOrigin] = useState("");
+  const [filterByDestination, setFilterByDestination] = useState("");
+
+  const filteredPriceOffers = priceOffers.filter(
+    (priceOffer) =>
+      (filterByOrigin !== "" ? filterByOrigin === priceOffer.origin : true) &&
+      (filterByDestination !== ""
+        ? filterByDestination === priceOffer.destination
+        : true)
+  );
+
   return (
     <>
       <Heading fontSize={22} mt={16} mb={4}>
         Filters
       </Heading>
-
-      <Card mb={8}>
-        <CardBody>
-          <form>
-            <FormControl>
-              <FormLabel fontWeight={800} fontSize={16}>
-                Filter by origin
-              </FormLabel>
-              <Select
-                bg={"white"}
-                icon={<RiArrowDownSFill />}
-                placeholder="Select origin"
-                borderColor={"gray.500"}
-                fontSize={14}
-              >
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </Select>
-            </FormControl>
-
-            <FormControl mt={6}>
-              <FormLabel fontWeight={800} fontSize={16}>
-                Filter by destination
-              </FormLabel>
-              <Select
-                bg={"white"}
-                icon={<RiArrowDownSFill />}
-                placeholder="Select origin"
-                borderColor={"gray.500"}
-                fontSize={14}
-              >
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </Select>
-            </FormControl>
-          </form>
-        </CardBody>
-      </Card>
+      <FlightFilter
+        priceOffers={priceOffers}
+        onOriginChange={(origin) => setFilterByOrigin(origin)}
+        onDestinationChange={(destination) =>
+          setFilterByDestination(destination)
+        }
+      ></FlightFilter>
 
       <Heading fontSize={22} mt={16} mb={4}>
         Results
       </Heading>
-
-      {priceOffers.map((priceOffer) => (
+      {filteredPriceOffers.map((priceOffer) => (
         <FlightCard key={priceOffer.uuid} {...priceOffer}></FlightCard>
       ))}
+
+      {filteredPriceOffers.length === 0 && (
+        <Card mb={4}>
+          <CardBody>
+            <Text fontSize={"large"} as="b" color={"pink.700"}>
+              Sorry, currently there are no flights available from{" "}
+              {filterByOrigin} to {filterByDestination}.
+            </Text>
+          </CardBody>
+        </Card>
+      )}
     </>
   );
 }
